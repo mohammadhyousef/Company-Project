9bce0681f52e817b2b686ecfa6feb6ac5bdcc6eb
mohammadhyousef <105796804+mohammadhyousef@users.noreply.github.com>
2025-03-20 01:45:20 +0300

    Update models.py

--- projects/models.py
+++ projects/models.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User  # Assuming you are using the default User model
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='projects/')
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
